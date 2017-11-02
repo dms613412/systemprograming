01493b3e29266f889c70c54cdc67f9968bd55310
sysprog <dms613412@naver.com>
2017-11-03 01:00:58 +0900

    glibc

---
+++ glibc_compile/glibc/build/elf/trusted-dirs.h
@@ -0,0 +1,8 @@
+#define SYSTEM_DIRS \
+  "/sysprog2017/hw3/glibc/build/lib/"
+
+#define SYSTEM_DIRS_LEN \
+  33
+
+#define SYSTEM_DIRS_MAX_LEN	33
+#define DL_DST_LIB "lib"
---
+++ glibc_compile/glibc/build/runtime-linker.h
@@ -0,0 +1,1 @@
+#define RUNTIME_LINKER "/sysprog2017/hw3/glibc/build/lib/ld-linux-x86-64.so.2"
